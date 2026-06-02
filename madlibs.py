d53489b72cc76f28f631542a30201969a89c73e3
AP24110010238 <keerthi4193@gmail.com>
2026-06-02 20:23:53 +0530

    Add files via upload

---
+++ madlibs.py
@@ -0,0 +1,19 @@
+#madlibs game
+
+print(f"Today I went to a (1.adjective1) Park.")
+print(f"In the Park I saw a (2.noun1).")
+print(f"(3.noun1) was (4.adjective2) and (5.verb1).")
+print(f"I was (6.adjective3).")
+
+adjective1= input("Fill 1:")
+noun1=input("Fill 2 & 3:")
+
+adjective2=input("Fill 4:")
+verb1=input("Fill 5:")
+adjective3=input("Fill 6:")
+
+
+print(f"Today I went to a {adjective1} Park.")
+print(f"In the Park I saw a {noun1}.")
+print(f"{noun1} was {adjective2} and {verb1}.")
+print(f"I was {adjective3}.")
