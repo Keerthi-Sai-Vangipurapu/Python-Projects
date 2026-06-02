435ebe7c55708265500088667b008f6bcac4109d
AP24110010238 <keerthi4193@gmail.com>
2026-06-02 21:01:39 +0530

    Create Weight_calc.py

---
+++ Weight_calc.py
@@ -0,0 +1,15 @@
+#Weight Calculator
+
+weight=float(input("Enter your Weight: "))
+unit=input("Kilograms or Pounds ?(K or L):")
+
+if(unit=="K"):
+    weight*=2.204
+    unit="Lbs."
+elif(unit=="L"):
+    weight/=2.204
+    unit="Kgm"
+else:
+    print(f"{unit} was not Valid.")
+
+print(f"The Weight: {weight} {unit}")
